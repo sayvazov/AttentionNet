e3cd9d8d87af04e366c8d7704e5cd71815097554
sayvazov <sayvazov@gmail.com>
2016-03-07 15:31:39 -0800

    Removed test files

--- AttentionNet/tensortest.py
+++
@@ -1,9 +0,0 @@
-import theano.tensor as T
-import theano
-import numpy as np
-
-A = theano.shared(np.arange(81).reshape(3,3,3,3))
-B = A[:, :, 0:2, 0:2] 
-
-print("A", A.eval())
-print("B", B.eval())
